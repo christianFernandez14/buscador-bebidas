59cdbd4a38832f1056ba6b116572058520bf02f1
Christian Fernandez <christian14@gmail.com>
2023-12-06 09:52:40 -0300

    Agregando los datos al formulario / Usando state local / Validando formilario (352)

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,64 +1,101 @@
-import { Button, Form, Row, Col, FormGroup, FormLabel, FormControl, FormSelect } from 'react-bootstrap'
+import { useState } from "react";
+import { Button, Form, Row, Col, FormGroup, FormLabel, FormControl, FormSelect, Alert } from 'react-bootstrap'
 import useCategorias from '../hooks/useCategorias'
 
 const Formulario = () => {
 
+  const [busqueda, setbusqueda] = useState(
+    {
+      nombre: '',
+      categoria: ''
+    }
+  )
+
+  const [alerta, setAlerta] = useState('')
+
   const { categorias } = useCategorias()
 
-  console.log(categorias)
 
+  const handleBusqueda = e => {
+    setbusqueda({
+      ...busqueda,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    if (Object.values(busqueda).includes('')) {
+      setAlerta('Todos los campos son obligartorios')
+
+      return;
+    }
+    setAlerta('')
+  }
 
   return (
-    <Form>
-      <Row>
-        <Col md={6}>
-          <FormGroup className='mb-3'>
-            <FormLabel htmlFor='nombre'>Nombre Bebida</FormLabel>
+    <>
+    
+      <Form
+        onSubmit={handleSubmit}
+      >
+        {alerta && <Alert variant="danger" className="text-center">{alerta}</Alert>}
+        <Row>
+          <Col md={6}>
+            <FormGroup className='mb-3'>
+              <FormLabel htmlFor='nombre'>Nombre Bebida</FormLabel>
 
-            <FormControl
-              id={'nombre'}
-              type='text'
-              placeholder='Ej: Tequila, Vodka, etc..'
-              name='nombre'
+              <FormControl
+                id={'nombre'}
+                type='text'
+                placeholder='Ej: Tequila, Vodka, etc..'
+                name='nombre'
+                value={busqueda.nombre}
+                onChange={handleBusqueda}
 
-            />
-          </FormGroup>
-        </Col>
-        <Col md={6}>
-          <FormGroup className='mb-3'>
-            <FormLabel htmlFor='categoria'>Nombre Bebida</FormLabel>
+              />
+            </FormGroup>
+          </Col>
+          <Col md={6}>
+            <FormGroup className='mb-3'>
+              <FormLabel htmlFor='categoria'>Categoria Bebida</FormLabel>
 
-            <FormSelect
-              id='categoria'
-              name='categoria'
+              <FormSelect
+                id='categoria'
+                name='categoria'
+                value={busqueda.categoria}
+                onChange={handleBusqueda}
+              >
+                <option value="">- Selecciona la Categoría -</option>
+                {
+                  categorias.map(categoria => (
+                    <option
+                      key={categoria.strCategory}
+                      value={categoria.strCategory}
+                    >{categoria.strCategory}</option>
+                  ))
+
+                }
+              </FormSelect>
+            </FormGroup>
+          </Col>
+        </Row>
+
+        <Row className='justify-content-end'>
+          <Col md={3}>
+            <Button
+              variant='danger'
+              className='text-uppercase w-100'
+              type="submit"
             >
-              <option value="">- Selecciona la Categoría -</option>
-              {
-                categorias.map(categoria => (
-                  <option
-                    key={categoria.strCategory}
-                    value={categoria.strCategory}
-                  >{categoria.strCategory}</option>
-                ))
+              Buscar Bebidas
+            </Button>
+          </Col>
+        </Row>
 
-              }
-            </FormSelect>
-          </FormGroup>
-        </Col>
-      </Row>
-
-      <Row className='justify-content-end'>
-        <Col md={3}>
-          <Button
-          variant='danger'
-          className='text-uppercase w-100'
-          >
-            Buscar Bebidas
-          </Button>
-        </Col>
-      </Row>
-
-    </Form>
+      </Form>
+    </>
   )
 }
 
